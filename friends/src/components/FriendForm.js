b9f77a81fde8e88db0cfd70325ccdcd3e78f1216
josephriosio <blankworldwide@gmail.com>
2019-04-02 13:15:36 -0700

    added handle changes to friendform

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 
-const FriendForm = () => {
-  return (
-    <div>
-      <h1>enter new friend</h1>
-      <form>
-        <input name="name" placeholder="enter name..." type="text" />
-        <input name="age" placeholder="enter age..." type="text" />
-        <input name="email" placeholder="enter email..." type="email" />
-        <button>submit</button>
-      </form>
-    </div>
-  );
-};
+class FriendForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "",
+      email: "",
+      age: ""
+    };
+  }
+
+  handleChanges = e => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>enter new friend</h1>
+        <form>
+          <input
+            onChange={this.handleChanges}
+            value={this.state.name}
+            name="name"
+            placeholder="enter name..."
+            type="text"
+          />
+          <input
+            onChange={this.handleChanges}
+            value={this.state.age}
+            name="age"
+            placeholder="enter age..."
+            type="text"
+          />
+          <input
+            onChange={this.handleChanges}
+            value={this.state.email}
+            name="email"
+            placeholder="enter email..."
+            type="email"
+          />
+          <button>submit</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default FriendForm;

7a11842ef02bf86295015d15915fcb65938c5d74
josephriosio <blankworldwide@gmail.com>
2019-04-02 13:34:21 -0700

    added axios post request and add a friend to server

--- friends/src/App.js
+++ friends/src/App.js
@@ -25,6 +25,16 @@ class App extends Component {
         this.setState({ error: err });
       });
   }
+
+  addFriend = friend => {
+    axios
+      .post("http://localhost:5000/friends", friend)
+      .then(res => this.setState({ friends: res.data }))
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   render() {
     return (
       <div>
@@ -41,7 +51,10 @@ class App extends Component {
             <FriendsList {...props} friends={this.state.friends} />
           )}
         />
-        <Route path="/form" component={FriendForm} />
+        <Route
+          path="/form"
+          render={props => <FriendForm {...props} addFriend={this.addFriend} />}
+        />
       </div>
     );
   }
--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -5,16 +5,33 @@ class FriendForm extends Component {
     super();
 
     this.state = {
-      name: "",
-      email: "",
-      age: ""
+      friend: {
+        name: "",
+        email: "",
+        age: ""
+      }
     };
   }
 
   handleChanges = e => {
-    e.preventDefault();
     this.setState({
-      [e.target.name]: e.target.value
+      friend: {
+        ...this.state.friend,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  addFriend = e => {
+    e.preventDefault();
+
+    this.props.addFriend(this.state.friend);
+    this.setState({
+      friend: {
+        name: "",
+        email: "",
+        age: ""
+      }
     });
   };
 
@@ -22,29 +39,29 @@ class FriendForm extends Component {
     return (
       <div>
         <h1>enter new friend</h1>
-        <form>
+        <form onSubmit={this.addFriend}>
           <input
             onChange={this.handleChanges}
-            value={this.state.name}
+            value={this.state.friend.name}
             name="name"
             placeholder="enter name..."
             type="text"
           />
           <input
             onChange={this.handleChanges}
-            value={this.state.age}
+            value={this.state.friend.age}
             name="age"
             placeholder="enter age..."
             type="text"
           />
           <input
             onChange={this.handleChanges}
-            value={this.state.email}
+            value={this.state.friend.email}
             name="email"
             placeholder="enter email..."
             type="email"
           />
-          <button>submit</button>
+          <button type="submit">submit</button>
         </form>
       </div>
     );
